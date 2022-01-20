34656d4befa32f2efc422215b2a587eab8f82c1d
Nama-Venkata-Vishwak <84949138+Nama-Venkata-Vishwak@users.noreply.github.com>
2022-01-20 18:37:24 +0530

    Create grid.java

---
+++ grid.java
@@ -0,0 +1,27 @@
+import java.util.*
+class nama {
+	static int numberOfPaths(int m, int n)
+	{
+		int count[][] = new int[m][n];
+
+		for (int i = 0; i < m; i++)
+			count[i][0] = 1;
+
+		for (int j = 0; j < n; j++)
+			count[0][j] = 1;
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++)
+				count[i][j] = count[i - 1][j] + count[i][j - 1]; //+ count[i-1][j-1];
+		}
+		return count[m - 1][n - 1];
+	}
+
+	public static void main(String args[])
+	{
+    Scanner sc=new Scanner(System.in);
+    int m=sc.nextInt()
+    int n=sc.nextInt()
+		System.out.println(numberOfPaths(m, n));
+	}
+}
